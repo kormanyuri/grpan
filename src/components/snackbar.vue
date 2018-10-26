f7533a5e717610ab4ec223d26d2a68ec5adbf2c0
pasha <fastist@gmail.com>
2018-10-26 08:51:32 -0600

    change snackbar

--- src/components/snackbar.vue
+++ src/components/snackbar.vue
@@ -2,10 +2,17 @@
     <div class="snackbar-container">
         <transition name="snackbar">
             <div class="snackbar" v-if="show">
-                <div class="snackbar-text">
-                    We use cookies to give you the best experience on our website. By continuing to use our website without changing
-                    <br> your cookie settings, you agree to our use of cookies in accordance with our updated
-                    <router-link :to="{}">Cookie Policy</router-link>
+                <div class="md-layout" style="width: 100%">
+                    <div class="md-layout-item md-medium-size-95 md-large-size-80 md-xlarge-size-80">
+                        <div class="snackbar-text">
+                            We use cookies to give you the best experience on our website. By continuing to use our website without changing
+                            <br> your cookie settings, you agree to our use of cookies in accordance with our updated
+                            <router-link :to="{}">Cookie Policy</router-link>
+                        </div>
+                    </div>
+                    <div class="md-layout-item md-medium-size-100 md-large-size-20 md-xlarge-size-20 wrap-btn">
+                        <md-button class="md-primary btn-1">{{$t("message.Accept")}}</md-button>
+                    </div>
                 </div>
                 <div class="snackbar-action">
                     <md-button class="md-icon-button close-menu-btn" @click="show = false" >
@@ -48,6 +55,21 @@
 
 <style lang="scss" scoped>
 
+    .btn-1 {
+        height: 50px;
+        min-width: 150px;
+        background-color: #ffb825;
+        color: #ffffff !important;
+        border-radius: 4px;
+        font-size: 1rem;
+        text-transform: none;
+        padding: 0 8px;
+        font-family: 'Open Sans', sans-serif;
+        font-weight: bold;
+        letter-spacing: 0.7px;
+        box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.24);
+    }
+
     .snackbar-container {
         position: fixed;
         overflow: hidden;
@@ -70,13 +92,13 @@
 
     .snackbar {
         display: flex;
-        align-items: center;
+        /*align-items: center;*/
         width: 100%;
         min-height: 48px;
         padding: 16px 24px;
         border-radius: 2px;
-        opacity: 0.81;
-        background-color: #000000;
+        /*opacity: 0.81;*/
+        background-color: rgba(0,0,0,0.81);
     }
 
     .snackbar-text {
@@ -84,6 +106,7 @@
         color: white;
         text-align: center;
         margin: 0 auto;
+        padding-top: 10px;
     }
 
     .snackbar-action {
@@ -99,6 +122,26 @@
     .snackbar-enter, .snackbar-leave-to {
         transform: translateY(60px);
         opacity: 0;
+    }
+
+    @media(min-width: 1280px) {
+        .snackbar {
+            /*display: flex;*/
+            align-items: center;
+        }
+        .wrap-btn {
+            text-align: left;
+            /*padding-top: 15px;*/
+        }
+    }
+    @media(max-width: 1281px) {
+        .wrap-btn {
+            text-align: center;
+            padding-top: 15px;
+        }
+        .snackbar-action {
+            padding-left: 0px;
+        }
     }
 
 </style>
