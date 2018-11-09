d4af58b88e245267b221bae9e87caea178c07388
korman <korman.yuri@gmail.com>
2018-11-09 12:18:58 +0200

    add cookie police

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  backendUrl: 'https://greenpandagames.com/',
+  backendUrl: 'http://greenpanda.ceant.net/',
   organizationReference: '8Vgz2M1',
   enableMultiLanguage: true,
   sitekey: '6LfJ93cUAAAAAPW6ELmmZsHgsa0eutsFfewshMBa'
